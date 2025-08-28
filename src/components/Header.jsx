50d01a650e00a7a3c4a47bfac15dd440a05202f6
paulina-pacheco-o <paulimichi05@gmail.com>
2025-08-28 18:26:08 +0200

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-100 headerTitle" >
+          <h1>Learn Web development</h1>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Header
